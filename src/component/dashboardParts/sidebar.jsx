d49b7c4f0737b07f631af781da2eb5f2035402a8
orlamee <meetmichaelakindele@gmail.com>
2023-07-17 15:10:44 +0100

    fixes

--- src/component/dashboardParts/sidebar.jsx
+++ src/component/dashboardParts/sidebar.jsx
@@ -24,75 +24,75 @@ function Sidebar() {
           <a className="" href="/dashboard"><Icon icon="radix-icons:dashboard" className="me-3 nme-1 fs-4" />Dashboard</a>
           <div className="item">
             <a className="sub-btn"><Icon icon="ri:building-2-line" className="me-3 nme-1 fs-4" />Loans <i className="bi bi-chevron-right dropdown"></i></a>
-            <div class="sub-menu">
-              <a href="" class="sub-item">Dashboard</a>
-              <a href="" class="sub-item">All Loans</a>
-              <a href="" class="sub-item">Applications</a>
-              <a href="" class="sub-item">Manage products</a>
-              <a href="" class="sub-item">Bulk loan payments</a>
-              <a href="" class="sub-item">Bulk import loan</a>
-              <a href="" class="sub-item">Calculator</a>
-              <a href="" class="sub-item">Settings</a>
-              <a href="" class="sub-item">Report</a>
+            <div className="sub-menu">
+              <a href="" className="sub-item">Dashboard</a>
+              <a href="" className="sub-item">All Loans</a>
+              <a href="" className="sub-item">Applications</a>
+              <a href="" className="sub-item">Manage products</a>
+              <a href="" className="sub-item">Bulk loan payments</a>
+              <a href="" className="sub-item">Bulk import loan</a>
+              <a href="" className="sub-item">Calculator</a>
+              <a href="" className="sub-item">Settings</a>
+              <a href="" className="sub-item">Report</a>
             </div>
           </div>
           <div className="item">
             <a className="sub-btn"><Icon icon="clarity:users-line" className="me-3 nme-1 fs-4" />User management <i className="bi bi-chevron-right dropdown"></i></a>
-            <div class="sub-menu">
-              <a href="" class="sub-item">Manage users</a>
-              <a href="" class="sub-item">Import users</a>
-              <a href="" class="sub-item">Settings</a>
+            <div className="sub-menu">
+              <a href="" className="sub-item">Manage users</a>
+              <a href="" className="sub-item">Import users</a>
+              <a href="" className="sub-item">Settings</a>
             </div>
           </div>
           <div className="item">
             <a className="sub-btn"><Icon icon="ep:user" className="me-3 nme-1 fs-4" />Agent management <i className="bi bi-chevron-right dropdown"></i></a>
-            <div class="sub-menu">
-              <a href="" class="sub-item">Manage agent</a>
-              <a href="" class="sub-item">Import agent</a>
-              <a href="" class="sub-item">Settings</a>
+            <div className="sub-menu">
+              <a href="" className="sub-item">Manage agent</a>
+              <a href="" className="sub-item">Import agent</a>
+              <a href="" className="sub-item">Settings</a>
             </div>
           </div>
           <div className="item">
             <a className="sub-btn"><Icon icon="material-symbols:business-center-outline" className="me-3 nme-1 fs-4" />Business management <i className="bi bi-chevron-right dropdown"></i></a>
-            <div class="sub-menu">
-              <a href="" class="sub-item">Manage company</a>
-              <a href="" class="sub-item">Onboarding company</a>
-              <a href="" class="sub-item">Settings</a>
-              <a href="" class="sub-item">Report</a>
+            <div className="sub-menu">
+              <a href="" className="sub-item">Manage company</a>
+              <a href="" className="sub-item">Onboarding company</a>
+              <a href="" className="sub-item">Settings</a>
+              <a href="" className="sub-item">Report</a>
             </div>
           </div>
           <div className="item">
             <a className="sub-btn"><Icon icon="jam:store" className="me-3 nme-1 fs-4" />Merchant management <i className="bi bi-chevron-right dropdown"></i></a>
-            <div class="sub-menu">
-              <a href="" class="sub-item">Manage merchants</a>
-              <a href="" class="sub-item">Inventory</a>
-              <a href="" class="sub-item">Settings</a>
-              <a href="" class="sub-item">Reports</a>
+            <div className="sub-menu">
+              <a href="" className="sub-item">Manage merchants</a>
+              <a href="" className="sub-item">Inventory</a>
+              <a href="" className="sub-item">Settings</a>
+              <a href="" className="sub-item">Reports</a>
             </div>
           </div>
           <a className="" href="/"><Icon icon="solar:card-linear" className="me-3 nme-1 fs-4" />Payment</a>
           <div className="item">
             <a className="sub-btn"><Icon icon="streamline:money-atm-card-3-deposit-money-payment-finance-atm-withdraw" className="me-3 nme-1 fs-4" />Savings <i className="bi bi-chevron-right dropdown"></i></a>
-            <div class="sub-menu">
-              <a href="" class="sub-item">Total deposit</a>
-              <a href="" class="sub-item">Total withdrawal</a>
+            <div className="sub-menu">
+              <a href="" className="sub-item">Total deposit</a>
+              <a href="" className="sub-item">Total withdrawal</a>
             </div>
           </div>
           <div className="item">
             <a className="sub-btn"><Icon icon="solar:shop-outline" className="me-3 nme-1 fs-4" />Market place <i className="bi bi-chevron-right dropdown"></i></a>
-            <div class="sub-menu">
-              <a href="" class="sub-item">Manage marketplace</a>
-              <a href="" class="sub-item">Offer available</a>
-              <a href="" class="sub-item">Inventory</a>
-              <a href="" class="sub-item">Settings</a>
+            <div className="sub-menu">
+              <a href="" className="sub-item">Manage marketplace</a>
+              <a href="" className="sub-item">Offer available</a>
+              <a href="" className="sub-item">Inventory</a>
+              <a href="" className="sub-item">Settings</a>
             </div>
           </div>
           <div className="item">
             <a className="sub-btn"><Icon icon="nimbus:marketing" className="me-3 nme-1 fs-4" />Marketing <i className="bi bi-chevron-right dropdown"></i></a>
-            <div class="sub-menu">
-              <a href="" class="sub-item">Bulk sms</a>
-              <a href="" class="sub-item">Email</a>
-              <a href="" class="sub-item">Push notification</a>
+            <div className="sub-menu">
+              <a href="" className="sub-item">Bulk sms</a>
+              <a href="" className="sub-item">Email</a>
+              <a href="" className="sub-item">Push notification</a>
             </div>
           </div>
           <a className="" href="/"><Icon icon="ion:wallet-outline" className="me-3 nme-1 fs-4" />Wallet</a>
