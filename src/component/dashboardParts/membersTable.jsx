3e7689882a678164a5abdcee968a51c828316a02
orlamee <meetmichaelakindele@gmail.com>
2023-09-17 15:52:54 +0100

    newfeature: Add Member Modal

--- src/component/dashboardParts/membersTable.jsx
+++ src/component/dashboardParts/membersTable.jsx
@@ -2,6 +2,9 @@
 import { Col, Form, Row } from "react-bootstrap";
 import SearchNotification from "./searchNotification";
 import Select from 'react-select'
+import React, { useState } from "react";
+import DataTable from "react-data-table-component";
+
 
 const options = [
   { value: 'Dashboard', label: 'Dashboard' },
@@ -10,10 +13,95 @@ const options = [
   { value: 'User-Management', label: 'User Management' },
   { value: 'Agent-Management', label: 'Agent Management' },
   { value: 'Business-Management', label: 'Business Management' },
-]
+];
+
+
+const columns = [
+  {
+    name: 'Name',
+    selector: 'fullName',
+    sortable: true,
+  },
+  {
+    name: 'Role',
+    selector: 'title',
+    sortable: true,
+  },
+  {
+    name: 'Email',
+    selector: 'email',
+    sortable: true,
+  },
+  {
+    name: 'Last Login',
+    selector: 'lastLogin',
+    sortable: true,
+  },
+  {
+    name: 'Action',
+    cell: (row) => (
+      <div className="dropdown">
+        <i
+          className="bi bi-three-dots"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        ></i>
+        <ul className="dropdown-menu details">
+          <li>
+            <a className="dropdown-item" href="/members/details">
+              <i className="bi bi-eye me-2"></i>View Member Details
+            </a>
+          </li>
+          <li>
+            <a
+              className="dropdown-item"
+              data-bs-toggle="modal"
+              data-bs-target="#delete-member"
+            >
+              <i className="bi bi-trash3 me-2 text-danger"></i>
+              Delete Member
+            </a>
+          </li>
+        </ul>
+      </div>
+    ),
+  },
+];
 
 
 function MemberTables() {
+  const [members, setMembers] = useState([]);
+  const [showAddMemberModal, setShowAddMemberModal] = useState(false);
+  const [newMemberData, setNewMemberData] = useState({
+    firstName: "",
+    lastName: "",
+    phoneNumber: "",
+    email: "",
+    password: "",
+    title: "",
+    permissions: [],
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  const handleAddMember = (event) => {
+    event.preventDefault();
+    setLoading(true);
+
+    // Simulate an API request delay (you can replace this with your actual API request)
+    setTimeout(() => {
+      const newMember = { ...newMemberData, lastLogin: '15/08/2023, 10:00am' };
+      setMembers([...members, newMember]);
+      setShowAddMemberModal(false);
+      setLoading(false);
+    }, 1500);
+  };
+
+  const data = members.map((member) => ({
+    ...member,
+    fullName: `${member.firstName} ${member.lastName}`,
+  }));
+
   return (
     <div className="content home">
       <SearchNotification />
@@ -29,8 +117,7 @@ function MemberTables() {
               </h5>
             </div>
             <button
-              data-bs-toggle="modal"
-              data-bs-target="#add-member"
+              onClick={() => setShowAddMemberModal(true)}
               className="btn-save float-end"
               style={{ marginTop: "-38px" }}
             >
@@ -43,227 +130,26 @@ function MemberTables() {
         className="row mt-2 mx-5 members bg-white py-5 px-2"
         style={{ borderRadius: "0 0 20px 20px" }}
       >
-        <div className="col">
-          <table
-            className="table table-responsive"
-            style={{ width: "100%" }}
-            id="application"
-          >
-            <thead className="bg-white rounded-pill">
-              <tr>
-                <th>Name</th>
-                <th>Role</th>
-                <th>Email</th>
-                <th>Last Login</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>John Doe</td>
-                <td>Admin</td>
-                <td>johndoe@gmail.com</td>
-                <td>15/08/2023, 10:00am</td>
-                <td>
-                  <div className="dropdown">
-                    <i
-                      className="bi bi-three-dots"
-                      data-bs-toggle="dropdown"
-                      aria-expanded="false"
-                    ></i>
-                    <ul class="dropdown-menu details">
-                      <li>
-                        <a className="dropdown-item" href="/members/details">
-                          <i className="bi bi-eye me-2"></i>View Member Details
-                        </a>
-                      </li>
-                      <li>
-                        <a
-                          className="dropdown-item"
-                          data-bs-toggle="modal"
-                          data-bs-target="#delete-member"
-                        >
-                          <i className="bi bi-trash3 me-2 text-danger"></i>
-                          Delete Member
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td>John Doe</td>
-                <td>Admin</td>
-                <td>johndoe@gmail.com</td>
-                <td>15/08/2023, 10:00am</td>
-                <td>
-                  <div className="dropdown">
-                    <i
-                      className="bi bi-three-dots"
-                      data-bs-toggle="dropdown"
-                      aria-expanded="false"
-                    ></i>
-                    <ul class="dropdown-menu details">
-                      <li>
-                        <a className="dropdown-item" href="/members/details">
-                          <i className="bi bi-eye me-2"></i>View Member Details
-                        </a>
-                      </li>
-                      <li>
-                        <a
-                          className="dropdown-item"
-                          data-bs-toggle="modal"
-                          data-bs-target="#delete-member"
-                        >
-                          <i className="bi bi-trash3 me-2 text-danger"></i>
-                          Delete Member
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td>John Doe</td>
-                <td>Admin</td>
-                <td>johndoe@gmail.com</td>
-                <td>15/08/2023, 10:00am</td>
-                <td>
-                  <div className="dropdown">
-                    <i
-                      className="bi bi-three-dots"
-                      data-bs-toggle="dropdown"
-                      aria-expanded="false"
-                    ></i>
-                    <ul class="dropdown-menu details">
-                      <li>
-                        <a className="dropdown-item" href="/members/details">
-                          <i className="bi bi-eye me-2"></i>View Member Details
-                        </a>
-                      </li>
-                      <li>
-                        <a
-                          className="dropdown-item"
-                          data-bs-toggle="modal"
-                          data-bs-target="#delete-member"
-                        >
-                          <i className="bi bi-trash3 me-2 text-danger"></i>
-                          Delete Member
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td>John Doe</td>
-                <td>Admin</td>
-                <td>johndoe@gmail.com</td>
-                <td>15/08/2023, 10:00am</td>
-                <td>
-                  <div className="dropdown">
-                    <i
-                      className="bi bi-three-dots"
-                      data-bs-toggle="dropdown"
-                      aria-expanded="false"
-                    ></i>
-                    <ul class="dropdown-menu details">
-                      <li>
-                        <a className="dropdown-item" href="/members/details">
-                          <i className="bi bi-eye me-2"></i>View Member Details
-                        </a>
-                      </li>
-                      <li>
-                        <a
-                          className="dropdown-item"
-                          data-bs-toggle="modal"
-                          data-bs-target="#delete-member"
-                        >
-                          <i className="bi bi-trash3 me-2 text-danger"></i>
-                          Delete Member
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td>Maryanne Obidi</td>
-                <td>Admin</td>
-                <td>johndoe@gmail.com</td>
-                <td>15/08/2023, 10:00am</td>
-                <td>
-                  <div className="dropdown">
-                    <i
-                      className="bi bi-three-dots"
-                      data-bs-toggle="dropdown"
-                      aria-expanded="false"
-                    ></i>
-                    <ul class="dropdown-menu details">
-                      <li>
-                        <a className="dropdown-item" href="/members/details">
-                          <i className="bi bi-eye me-2"></i>View Member Details
-                        </a>
-                      </li>
-                      <li>
-                        <a
-                          className="dropdown-item"
-                          data-bs-toggle="modal"
-                          data-bs-target="#delete-member"
-                        >
-                          <i className="bi bi-trash3 me-2 text-danger"></i>
-                          Delete Member
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td>John Doe</td>
-                <td>Admin</td>
-                <td>johndoe@gmail.com</td>
-                <td>15/08/2023, 10:00am</td>
-                <td>
-                  <div className="dropdown">
-                    <i
-                      className="bi bi-three-dots"
-                      data-bs-toggle="dropdown"
-                      aria-expanded="false"
-                    ></i>
-                    <ul class="dropdown-menu details">
-                      <li>
-                        <a className="dropdown-item" href="/members/details">
-                          <i className="bi bi-eye me-2"></i>View Member Details
-                        </a>
-                      </li>
-                      <li>
-                        <a
-                          className="dropdown-item"
-                          data-bs-toggle="modal"
-                          data-bs-target="#delete-member"
-                        >
-                          <i className="bi bi-trash3 me-2 text-danger"></i>
-                          Delete Member
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+        <div className="col" style={{minHeight: "600px"}}>
+          <DataTable
+            title="Members"
+            columns={columns}
+            data={data}
+            pagination
+            selectableRows
+          />
         </div>
       </div>
-      {/* Add Members */}
+      {/* Add Members Modal */}
       <div
-        className="modal fade modal-m"
+        className={`modal fade modal-m ${showAddMemberModal ? "show" : ""}`}
         id="add-member"
         data-bs-backdrop="static"
         data-bs-keyboard="false"
-        tabindex="-1"
+        tabIndex="-1"
         aria-labelledby="staticBackdropLabel"
-        aria-hidden="true"
+        aria-hidden={!showAddMemberModal}
+        style={{ display: showAddMemberModal ? "block" : "none" }}
       >
         <div className="modal-dialog modal-dialog-centered modal-lg">
           <div className="modal-content">
@@ -274,35 +160,70 @@ function MemberTables() {
                 className="btn-close"
                 data-bs-dismiss="modal"
                 aria-label="Close"
+                onClick={() => setShowAddMemberModal(false)}
               ></button>
             </div>
             <div className="modal-body">
               <div className="inner-card">
                 <div className="row">
                   <div className="col-sm-12 add-m">
-                    <Form>
+                    <Form onSubmit={handleAddMember}>
                       <Row className="mb-4">
                         <Col xs={6}>
-                          <Form.Control type="text" placeholder="First Name" />
+                          <Form.Control
+                            type="text"
+                            placeholder="First Name"
+                            name="firstName"
+                            onChange={(e) => setNewMemberData({ ...newMemberData, firstName: e.target.value })}
+                            required
+                          />
                         </Col>
                         <Col>
-                          <Form.Control placeholder="Last Name" />
+                          <Form.Control
+                            placeholder="Last Name"
+                            name="lastName"
+                            onChange={(e) => setNewMemberData({ ...newMemberData, lastName: e.target.value })}
+                            required
+                          />
                         </Col>
                       </Row>
                       <Row className="mb-4">
                         <Col xs={6}>
-                          <Form.Control type="tel" placeholder="Phone Number" />
+                          <Form.Control
+                            type="tel"
+                            placeholder="Phone Number"
+                            name="phoneNumber"
+                            onChange={(e) => setNewMemberData({ ...newMemberData, phoneNumber: e.target.value })}
+                            required
+                          />
                         </Col>
                         <Col>
-                          <Form.Control placeholder="Email" />
+                          <Form.Control
+                            type="email"
+                            placeholder="Email"
+                            name="email"
+                            onChange={(e) => setNewMemberData({ ...newMemberData, email: e.target.value })}
+                            required
+                          />
                         </Col>
                       </Row>
                       <Row className="mb-4">
                         <Col xs={6}>
-                          <Form.Control type="password" placeholder="Password" />
+                          <Form.Control
+                            type="password"
+                            placeholder="Password"
+                            name="password"
+                            onChange={(e) => setNewMemberData({ ...newMemberData, password: e.target.value })}
+                            required
+                          />
                         </Col>
                         <Col>
-                          <Form.Control placeholder="Title" />
+                          <Form.Control
+                            placeholder="Title"
+                            name="title"
+                            onChange={(e) => setNewMemberData({ ...newMemberData, title: e.target.value })}
+                            required
+                          />
                         </Col>
                       </Row>
                       <Row className="mb-4">
@@ -312,18 +233,27 @@ function MemberTables() {
                       </Row>
                       <Row className="mb-4">
                         <Col xs={12}>
-                          <Select 
+                          <Select
                             options={options}
                             isMulti
                             className="basic-multi-select"
                             placeholder="Select Permission"
-                           />
+                            name="permissions"
+                            required
+                            onChange={(selectedOptions) => setNewMemberData({ ...newMemberData, permissions: selectedOptions })}
+                          />
                         </Col>
                       </Row>
                       <div className="text-end">
-                        <button type="button" className="btn-save">
-                          Add Member
-                        </button>
+                        {loading ? (
+                          <div className="spinner-border text-primary" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                          </div>
+                        ) : (
+                          <button type="submit" className="btn-save">
+                            Add Member
+                          </button>
+                        )}
                       </div>
                     </Form>
                   </div>
@@ -333,14 +263,13 @@ function MemberTables() {
           </div>
         </div>
       </div>
-      
       {/* Delete Member */}
       <div
         className="modal fade vprogress"
         id="delete-member"
         data-bs-backdrop="static"
         data-bs-keyboard="false"
-        tabindex="-1"
+        tabIndex="-1"
         aria-labelledby="staticBackdropLabel"
         aria-hidden="true"
       >
